ef95fee53e631b778a1ecab495bc7c4e887f2424
xuxin1955 <x2874836915@qq.com>
2026-05-11 00:16:45 +0800

    update

--- diy-part5.sh
+++ diy-part5.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+# turboacc
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
 # hlk7620A dts
 mkdir -p target/linux/ramips/dts/
 cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7620a_zbtlink_zbt-we826.dtsi" "target/linux/ramips/dts/mt7620a_zbtlink_zbt-we826.dtsi"
